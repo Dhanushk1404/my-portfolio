2008f994cd54158f12b3061a95451a6aa737513b
Dhanushk1404 <dhanushk.22cse@kongu.edu>
2025-01-15 19:22:12 +0530

    home page altered

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import About from './components/About'
+import Home from './components/Home'
 
 const App = () => {
   return (
     <div>
+      <Home/>
       <About/>
       <h1>Hi !!</h1>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <div className="max-w-[85%] mx-auto font-inter grid grid-cols-1 md:grid-cols-2 gap-8 items-center  min-h-screen">
         <div className="order-2 lg:order-1 flex flex-col items-center md:items-start justify-start space-y-5 text-center md:text-left">
           <h1 className="text-3xl md:text-5xl font-bold">
-            I’m <span className="text-[#fe5617]">Mohammed Imran</span>
+            I’m <span className="text-[#fe5617]">Dhanush K</span>
           </h1>
           <h2 className="text-2xl md:text-3xl font-bold flex gap-2">
             <span className="text-[#fe5617]">I’m a</span>
@@ -38,15 +38,15 @@ const Home = () => {
           <div className=" flex flex-col space-y-3 md:space-y-0 md:flex-row md:gap-6 text-lg md:text-xl font-semibold ">
             <div className="flex items-center gap-3">
               <FaLocationDot className="text-[#fe5617]" size={20} />
-              <span>Pondicherry</span>
+              <span>Namakkal</span>
             </div>
             <div className="flex items-center gap-3">
               <FaPhoneAlt className="text-[#fe5617]" size={20} />
-              <span>9092595881</span>
+              <span>9677459394</span>
             </div>
             <div className="flex items-center gap-3">
               <FaEnvelope className="text-[#fe5617]" size={20} />
-              <span>immucoder@gmail.com</span>
+              <span>dhanushkanagaraj007@gmail.com</span>
             </div>
           </div>
          
