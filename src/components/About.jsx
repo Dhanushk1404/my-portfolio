ed5919cf13d356eba90a690c9abfdc80691fb563
Dhanushk1404 <dhanushk.22cse@kongu.edu>
2025-01-16 09:16:10 +0530

    color changes for all

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,32 +26,28 @@ import {
   import { TbBrandFramerMotion } from "react-icons/tb";
   import { DiVisualstudio } from "react-icons/di";
   import { IoLogoNpm, IoLogoVercel } from "react-icons/io5";
-  
   const About = () => {
     return (
-      <div id="about" className="bg-[#ffffff] min-h-screen flex items-center">
+      <div id="about" className="bg-[#333] min-h-screen flex items-center">
         <div className="max-w-[90%] md:max-w-[75%] mx-auto font-inter space-y-10 py-10">
-          
           <h1 className="text-4xl md:text-5xl font-bold text-center text-[#fe5617]">
             About Me
           </h1>
   
-     
-          <p className="text-lg md:text-xl text-gray-700 text-center leading-relaxed">
-  I’m <span className="font-bold text-[#fe5617]">Dhanush Kanagaraj</span>, 
-  an enthusiastic MERN stack developer eager to start my journey in the tech industry. 
-  With a strong foundation in web development, I’ve built several micro-projects to 
-  enhance my skills and understanding. As a learner, I’m deeply committed to continuous 
-  growth and passionate about crafting impactful solutions. Currently, I’m a pre-final 
-  year B.Tech student in Computer Science Engineering, excited to contribute to real-world 
-  projects and collaborate with innovative teams.
-</p>
-
-  
+          <p className="text-lg md:text-xl text-gray-200 text-center leading-relaxed">
+            I’m <span className="font-bold text-[#fe5617]">Dhanush Kanagaraj</span>,
+            an enthusiastic MERN stack developer eager to start my journey in the tech industry.
+            With a strong foundation in web development, I’ve built several micro-projects to
+            enhance my skills and understanding. As a learner, I’m deeply committed to continuous
+            growth and passionate about crafting impactful solutions. Currently, I’m a pre-final
+            year B.Tech student in Computer Science Engineering, excited to contribute to real-world
+            projects and collaborate with innovative teams.
+          </p>
         </div>
       </div>
     );
   };
   
   export default About;
+  
   
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,13 @@ import { FaLocationDot } from "react-icons/fa6";
 
 const Home = () => {
   return (
-    <div id="home" className="bg-[#FFFFFF]">
-      <div className="max-w-[85%] mx-auto font-inter grid grid-cols-1 md:grid-cols-2 gap-8 items-center  min-h-screen">
+    <div id="home" className="bg-[#333333]">
+      <div className="max-w-[85%] mx-auto font-inter grid grid-cols-1 md:grid-cols-2 gap-8 items-center min-h-screen">
         <div className="order-2 lg:order-1 flex flex-col items-center md:items-start justify-start space-y-5 text-center md:text-left">
-          <h1 className="text-3xl md:text-5xl font-bold">
+          <h1 className="text-3xl md:text-5xl font-bold text-white">
             I’m <span className="text-[#fe5617]">Dhanush K</span>
           </h1>
-          <h2 className="text-2xl md:text-3xl font-bold flex gap-2">
+          <h2 className="text-2xl md:text-3xl font-bold flex gap-2 text-white">
             <span className="text-[#fe5617]">I’m a</span>
             <Typewriter
               options={{
@@ -18,7 +18,7 @@ const Home = () => {
                   "Frontend Developer.",
                   "Backend Developer",
                   "Full Stack Developer.",
-                  "Devops Engineer",
+                  "Devops Engineer.",
                 ],
                 autoStart: true,
                 loop: true,
@@ -28,12 +28,12 @@ const Home = () => {
             />
           </h2>
           
-          <p className="text-lg md:text-xl text-gray-700 font-medium">
+          <p className="text-lg md:text-xl text-gray-300 font-medium">
               A passionate MERN stack enthusiast with a strong foundation in building user-friendly web applications. Eager to learn, grow, and contribute to dynamic teams. Enthusiastic about sharing knowledge as a tutor and content creator, fostering innovation and collaboration.
           </p>
 
           
-          <div className=" flex flex-col space-y-3 md:space-y-0 md:flex-row md:gap-6 text-lg md:text-xl font-semibold ">
+          <div className="flex flex-col space-y-3 md:space-y-0 md:flex-row md:gap-6 text-lg md:text-xl font-semibold text-white">
             <div className="flex items-center gap-3">
               <FaLocationDot className="text-[#fe5617]" size={20} />
               <span>Namakkal</span>
@@ -53,7 +53,6 @@ const Home = () => {
           </div>
         </div>
 
-       
         <div className="order-1 lg:order-2 flex justify-end items-center">
           <img
             className="w-[85%] rounded-[50px]"
